56f03a9b0af8f6b5b7ff8dfc1faa09a4e9605284
Clara Chen <clarachen@dhcp-10-250-174-225.harvard.edu>
2023-12-09 12:08:11 -0500

    Flexible width and height

--- js/barvis.js
+++ js/barvis.js
@@ -11,8 +11,8 @@ class BarVis {
         let vis= this;
 
         vis.margin = {top: 10, right: 0, bottom: 40, left: 100};
-        vis.width = 600;
-        vis.height = 500;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
--- js/bubblevis.js
+++ js/bubblevis.js
@@ -11,8 +11,8 @@ class BubbleVis {
         let vis= this;
 
         vis.margin = {top: 10, right: 0, bottom: 0, left: 0};
-        vis.width = 600;
-        vis.height = 600;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right,
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
