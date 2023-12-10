065d614862e7322d74c9c00b9421c6001cb67cc4
cchen2125 <cchen2125@gmail.com>
2023-12-10 17:42:42 -0500

    Dynamic sizing

--- js/scattervis.js
+++ js/scattervis.js
@@ -12,8 +12,8 @@ class ScatterVis {
         let vis = this;
 
         vis.margin = {top: 40, right: 10, bottom: 10, left: 40};
-        vis.width = 900;
-        vis.height = 550;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right,
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
